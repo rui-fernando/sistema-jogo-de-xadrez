cd9343dd044c13efa35a8fe7a3688c36acab7a53
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-08 15:57:34 -0300

    feat: cria camada chess e imprime tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,12 +5,12 @@ public class Board {
     private int rows;
     private int columns;
 
-    private Piece[][] pecas;
+    private Piece[][] pieces;
 
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
-        pecas = new Piece[rows][columns];
+        pieces = new Piece[rows][columns];
     }
 
     public int getRows() {
@@ -28,6 +28,15 @@ public class Board {
     public void setColumns(int columns) {
         this.columns = columns;
     }
+
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
 
 
 }
