b762b4964d87d2c94465b2a691d58b3dac30250b
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-08 16:40:44 -0300

    feat: insere peças no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +11,7 @@ public class ChessMatch {
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces (){
@@ -20,5 +24,11 @@ public class ChessMatch {
         }
 
         return matriz;
+    }
+
+    private void initialSetup(){
+        board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
+        board.placePiece(new King(board, Color.WHITE), new Position(6,1));
+        board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
     }
 }
