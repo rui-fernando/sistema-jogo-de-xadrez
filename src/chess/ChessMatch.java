9187b5ce96f96038d8a4337e7c1db45e2e9b58a1
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-09 15:30:39 -0300

    feat: implementa movimentos possíveis para um determinada peça no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -52,6 +52,10 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)){
             throw new ChessException("Não existe peça na posição de origem");
         }
+
+        if(!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("Não existem movimentos possíveis para a peça escolhida");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
