88654956f80d8587fd0b067230c97626173b3c93
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-09 14:58:00 -0300

    feat: implementa limpeza do terminal e exceções

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,11 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc){
 
         try {
@@ -42,7 +47,7 @@ public class UI {
             return new ChessPosition(column, row);
 
         } catch (RuntimeException e){
-            throw new InputMismatchException("Erro ao ler posição do xadrez; valores válidos são de 1 a 8.");
+            throw new InputMismatchException("Erro ao ler posição do xadrez; valores válidos são de a1 a h8.");
         }
     }
 
