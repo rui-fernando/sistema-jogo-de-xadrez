0c24a040a2caee32eb2dea9c6f5c135f4f662a60
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-09 01:03:21 -0300

    feat: implementa ChessException e ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -26,9 +25,13 @@ public class ChessMatch {
         return matriz;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
-        board.placePiece(new King(board, Color.WHITE), new Position(6,1));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
     }
 }
