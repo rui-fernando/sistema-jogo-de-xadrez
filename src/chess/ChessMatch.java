b7ef676409b9946ab04aedbee8318b7bc2f945d0
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-11 02:11:20 -0300

    feat: implementa contagem de movimentos de peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -118,7 +118,9 @@ public class ChessMatch {
     }
 
     private Piece makeMouve(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
+
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
 
@@ -131,7 +133,8 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece){
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if (capturedPiece != null){
